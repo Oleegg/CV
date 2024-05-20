f0e2983167441760bb89543828d31136a013b33a
OlegSenko <senkooleg@gmail.com>
2024-05-20 11:18:50 +0300

    add content

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useReducer } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import { Context, initialState, reducer } from "./reducers";
+import Main from "./components/Main/Main";
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -10,6 +11,7 @@ function App() {
     <Context.Provider value={{ state, dispatch }}>
       <div className="App">
         <Header />
+        <Main />
       </div>
     </Context.Provider>
   );
